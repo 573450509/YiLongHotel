a490df61abf769a694f5b87d73e917137d491249
ZYHZ <552153678@qq.com>
2015-11-23 18:06:09 +0800

    111

--- app/src/main/java/com/qf/yilonghotel/MainActivity.java
+++ app/src/main/java/com/qf/yilonghotel/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
     String url = "111";
+    String url1 = "111";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
