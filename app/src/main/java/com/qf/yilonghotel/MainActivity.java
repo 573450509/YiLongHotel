b2d08af6cdebd9ffe2e933dbcefcd3aa24e13bbf
LZY <573450509@qq.com>
2015-11-23 19:03:02 +0800

    改变布局

--- app/src/main/java/com/qf/yilonghotel/MainActivity.java
+++ app/src/main/java/com/qf/yilonghotel/MainActivity.java
@@ -3,10 +3,10 @@ package com.qf.yilonghotel;
 import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
 public class MainActivity extends Activity {
-
-
+RadioGroup rg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
