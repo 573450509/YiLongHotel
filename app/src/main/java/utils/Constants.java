f275a3d74840540d4daf38f82b41f43ee8156c62
LZY <573450509@qq.com>
2015-11-23 20:14:20 +0800

    添加URL地址的util

---
+++ app/src/main/java/utils/Constants.java
@@ -0,0 +1,23 @@
+package utils;
+
+/**
+ * Created by Ken on 2015/11/16.
+ */
+public interface Constants {
+
+    interface URL{
+        /**
+         * Home的URL
+         */
+        String CITY_SELECT_URL = "";
+    }
+
+    interface BUNDLE_KEY{
+        String CITY_OBJ_NAME = "city_obj";
+        String CITY_NAME_KEY = "cityname";
+        String CITY_ID_KEY = "cityid";
+
+        String FRAGMENT_NAV_DATA = "nav_data";
+    }
+
+}
